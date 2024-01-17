b47870abbb6f6c7e9a2f0ab94ebce60bc1e67f97
AdMuy <agu21010@byui.edu>
2024-01-16 21:45:16 -0600

    Completing prep 2 assigment.

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,42 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep2 World!");
+   {
+        Console.Write("What is grade percentage in numbers? ");
+        string percentage = Console.ReadLine();
+        int score = int.Parse(percentage);
+        string grade;
+
+        if (score >= 90)
+        {
+            grade = ("A");
+        }
+        else if (score >= 80)
+        {
+            grade = ("B");
+        }
+        else if (score >= 70)
+        {
+            grade = ("C");
+        }
+        else if (score >= 60)
+        {
+            grade = ("C");
+        }
+        else
+        {
+            grade = ("F");
+        }
+
+        Console.WriteLine($"Your grade is {grade}");
+
+        if (score >= 70)
+        {
+            Console.WriteLine("Congratulations you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Keep it up and study harder.");
+        }
     }
 }
