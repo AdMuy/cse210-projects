6aef71b4284668995b529b38a50c600341973bfa
AdMuy <agu21010@byui.edu>
2024-01-16 22:37:41 -0600

    Completing prep 3 assignment.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        Random randomGenerator = new Random();
+        int magicNumb = randomGenerator.Next(1, 100);
+        
+        int answer = -1;
+
+        do {
+            Console.Write("What is your guess?  ");
+            answer = int.Parse(Console.ReadLine());
+
+            if (magicNumb > answer)
+            {
+                Console.WriteLine("Go higher.");
+            }
+            else if (magicNumb < answer)
+            {
+                Console.WriteLine("Go lower.");
+            }
+            else
+            {
+                Console.WriteLine("That's the number.");
+            }
+        } while (answer != magicNumb);
     }
 }
