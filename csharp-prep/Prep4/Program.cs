df57f264df9b1f903cdef27b71cc1786592e206d
AdMuy <agu21010@byui.edu>
2024-01-17 22:36:25 -0600

    Completing prep 4 assignment.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,30 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        
+        List<int> numbers = new List<int>();
+        int number = -1;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+            else
+            {
+                numbers.Remove(number);
+            }
+
+        } while (number != 0);
+        Console.WriteLine(numbers.Sum());
+        Console.WriteLine(numbers.Average());
+        Console.WriteLine(numbers.Max());
     }
 }
