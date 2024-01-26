5b8e85f494ca1edfe23ebb4a3171614f148d53b1
AdMuy <agu21010@byui.edu>
2024-01-25 20:53:44 -0600

    Adding most of the program, I forgot to commit in the previous stages and only now that I broke how the list is saved I realized I need to commit my changes.

---
+++ prove/Develop02/Journal.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel;
+using System.IO; 
+public class Journal
+{
+    public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        foreach (Entry entry in _entries)
+            {
+                entry.Display();
+            }
+    }
+
+    public void SaveToFile(string file)
+    {
+        using (StreamWriter outputFile = new StreamWriter(file))
+        {
+            foreach (Entry entry in _entries)
+            {
+                outputFile.WriteLine($"Date: {entry._date} - Prompt: {entry._promptText} \n{entry._entryText}\n"+"~");
+            }
+        }
+    }
+    
+    public void LoadFromFile(string file)
+    {
+        string[] lines = File.ReadAllLines(file);
+
+        foreach (string line in lines)
+        {
+            int i = 0;
+            string[] parts = line.Split("~");
+            Console.WriteLine(parts[i]);
+            // Still need to know how to iterate through all of the parts
+            // And then have the loaded parts be added to the live
+            // So if Displayed is called after that, the correct lines appear.
+        }
+    }
+}
+
--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -3,7 +3,54 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Develop02 World!");
+    {        
+        PromptGenerator generator = new PromptGenerator();
+        Journal journal1 = new Journal();
+        Entry newEntry = new Entry();
+
+        DateTime theCurrentTime = DateTime.Now;
+        string dateText = theCurrentTime.ToShortDateString();
+
+        int option = -1;
+
+        do 
+        {
+            Console.WriteLine("Please select one of the following:");
+            Console.WriteLine("1. Write");
+            Console.WriteLine("2. Display");
+            Console.WriteLine("3. Load");
+            Console.WriteLine("4. Save");
+            Console.WriteLine("5. Quit");
+            Console.Write("What would you like to do? ");
+            option = int.Parse(Console.ReadLine());
+
+            if (option == 1)
+            {
+                newEntry._promptText = generator.GetRandomPrompt();
+                Console.WriteLine($"{newEntry._promptText}");
+                newEntry._entryText = Console.ReadLine();
+                newEntry._date = dateText;
+                journal1.AddEntry(newEntry);
+            }
+
+            else if (option == 2)
+            {
+                journal1.DisplayAll();
+            }
+
+            else if (option == 3)
+            {
+                Console.Write("What is the name of the file? ");
+                journal1.LoadFromFile(Console.ReadLine());
+                
+            }
+
+            else if (option == 4)
+            {
+                Console.Write("What is the name of the file? ");
+                journal1.SaveToFile(Console.ReadLine());
+            }
+        } while (option != 5);
+
     }
 }
